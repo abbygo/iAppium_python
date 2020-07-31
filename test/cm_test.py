af9134c26eb1d57971921c47001793076e96eaf6
abbygo <2448658821@qq.com>
2020-07-31 13:38:11 +0800

    目录问题

--- test/cm_test.py
+++ test/cm_test.py
@@ -20,7 +20,9 @@ class IAppium(unittest.TestCase):
         desired_caps['appActivity'] = config['desired_caps']['appActivity']
         desired_caps['automationName'] = config['desired_caps']['automationName']
         desired_caps['noReset'] = config['desired_caps']['noReset']
-        desired_caps['app'] = f'{os.path.split(os.getcwd())[0]}/app/ContactManager.apk'
+        # D:\jekins_slave_home\workspace\iappium / app / ContactManager.apk
+        desired_caps['app'] = f'{os.path.abspath(os.curdir)}/app/ContactManager.apk'
+        # desired_caps['app'] = f'{os.path.split(os.getcwd())[0]}/app/ContactManager.apk'
 
 
         self.driver = webdriver.Remote(appium_server_url, desired_caps)
